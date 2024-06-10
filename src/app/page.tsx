063e2d952a0f3ef118406317713fba76f4f4a1bb
Ryan Rudman <ryan.rudman@kinandcart.com>
2024-06-10 22:26:57 +0200

    remove wp blog links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Hero data={heroComponentData} />
-      <News data={newsComponentData} />
+      {/* <News data={newsComponentData} /> */}
     </main>
   );
 }
